5c0b0438d075ddc2b989168a4ad2795b36bb6c70
shimamura10 <shimamura-kazuki351@g.ecc.u-tokyo.ac.jp>
2023-06-25 02:31:17 +0900

    検索タグとリストに削除ボタン追加。主キーを通算の要素番号に。

--- main.js
+++ main.js
@@ -15,23 +15,38 @@
   const displaySearchTag = document.querySelector('#display-search-tag');
   const LSkey = 'informationList';
   const targetTags = [];
-  const informationList = JSON.parse(localStorage[LSkey]);
+  let informationList = {};
+  if (!(localStorage[LSkey] == "undefined" || typeof localStorage[LSkey] == "undefined")) {
+    informationList = JSON.parse(localStorage[LSkey]);
+  }
   const originInformation = {
     name: 'sampleName',
     url: 'sampleUrl',
     tag: ['sampleTag1', 'sampleTag2'],
   }
+  // informationList[nextKey()] = (Object.assign({}, originInformation));
 
-  informationList['key-1'] = (Object.assign({}, originInformation));
-
+  // タグ検索の追加ボタンを押したときの処理
   document.querySelector('#add-search-tag').addEventListener('click', () => {
+    // タグ要素の追加と表示
     const tag = document.createElement('a');
     tag.classList.add("searchTag");
     tag.text = inputSearchTag.value;
+    const delButton = document.createElement('input');
+    delButton.setAttribute('type', 'button');
+    delButton.setAttribute('value', 'X');
+    delButton.addEventListener('click', () => {
+      delButton.parentElement.remove();
+      const ind = targetTags.indexOf(delButton.parentElement.text);
+      targetTags.splice(ind, 1);
+      search();
+    })
+    tag.appendChild(delButton)
     displaySearchTag.appendChild(tag);
+    
     inputSearchTag.value = '';
     targetTags.push(tag.text);
-    display();
+    search();
   })
 
   // informationListの内容を表示
@@ -48,9 +63,6 @@
 
   // リストに情報を追加
   selectButton.addEventListener('click', () => {
-    // ウィンドウを閉じる
-    mask.classList.add('hidden');
-    addWindow.classList.add('hidden');
     // 情報を読み込む
     const cloneInformation = Object.assign({}, originInformation);
     cloneInformation.name = addName.value;
@@ -60,13 +72,16 @@
       const tag = addedTagSpace.children[i];
       cloneInformation.tag.push(tag.value);
     }
-    const key = 'key-' + String(Object.keys(informationList).length + 1);
+    // const key = 'key-' + String(Object.keys(informationList).length + 1);
+    const key = nextKey();
+    // const key = cloneInformation.name + cloneInformation.url;  //主キーは名前＋url
     informationList[key] = cloneInformation;
-    console.log(informationList);
 
     // 情報を書き込む
     displayInformation(cloneInformation, key);
-    save(informationList);
+    save();
+    // ウィンドウを閉じる
+    closeWindow();
   });
 
   document.querySelector('#add-tag-button').addEventListener('click', () => {
@@ -87,8 +102,6 @@
     const cloneTbody = originTbody.cloneNode(true);
     cloneTbody.id = key;
     mainTable.appendChild(cloneTbody);
-    console.log(cloneTbody.id);
-    // const name = document.querySelector('#sample .display-name');
     const name = document.querySelector(`#${key} .display-name`);
     name.text  = information.name;
     name.setAttribute("href", information.url);
@@ -97,32 +110,84 @@
       const newTag = document.createElement('a');
       newTag.classList.add('display-tag');
       newTag.text = infoTag;
-      // document.querySelector('#sample .tag-th').appendChild(newTag);
       document.querySelector(`#${key} .tag-th`).appendChild(newTag);
     });
+    // deleteボタンの追加
+    const deleteButton = document.querySelector(`#${key} .delete-button`);
+    deleteButton.addEventListener('click', () => {
+      const key = deleteButton.parentElement.id;
+      delete informationList[key];
+      deleteButton.parentElement.remove();
+      console.log(informationList);
+      save();
+      console.log(informationList);
+    })
   }
 
   // informationListをローカルストレージに保存
-  function save(informationList) {
+  function save() {
     localStorage.setItem(LSkey, JSON.stringify(informationList))
+    console.log(informationList);
   }
 
-  function display() {
-    Object.keys(informationList).forEach(key => {
-      let ok = false;
-      informationList[key].tag.forEach(tag => {
+  function search() {
+    console.log(targetTags.length);
+    if (targetTags.length == 0) { //絞り込みなし
+      console.log(informationList);
+      Object.keys(informationList).forEach(key => {
+        document.querySelector(`#${key}`).classList.remove('hidden');
+        console.log(document.querySelector(`#${key}`).classList);
+      });
+    } else { // 絞り込みあり
+      Object.keys(informationList).forEach(key => {
+        let ok = true;
         targetTags.forEach(targetTags => {
-          if (tag == targetTags) {
-            ok = true
+          let ok2 = false;
+          informationList[key].tag.forEach(tag => {
+            if (tag == targetTags) {
+              ok2 = true;
+              return;
+            }
+          });
+          if (!ok2) {
+            ok = false;
             return;
           }
         });
+        if (ok) {
+          document.querySelector(`#${key}`).classList.remove('hidden');
+        } else {
+          document.querySelector(`#${key}`).classList.add('hidden');
+        }
       });
-      if (ok) {
-        document.querySelector(`#${key}`).classList.remove('hidden');
-      } else {
-        document.querySelector(`#${key}`).classList.add('hidden');
-      }
-    });
+    }
   }
+
+  function nextKey() {
+    let tmp = localStorage['keyNum'];
+    if (typeof tmp == "undefined") {
+      tmp = 1;
+    }
+    localStorage['keyNum'] = Number(tmp) + 1;
+    return 'key-' + tmp;
+  }
+
+  function closeWindow() {
+    mask.classList.add('hidden');
+    addWindow.classList.add('hidden');
+    addName.value = '';
+    addUrl.value = '';
+    const l = addedTagSpace.children.length;
+    for (let i = 0; i < l; i++) {
+      if (i == 0) {
+        addedTagSpace.children[i].value = '';
+      } else {
+        addedTagSpace.lastElementChild.remove();
+      }
+    }
+  }
+
+  mask.addEventListener('click', () => {
+    closeWindow();
+  })
 }

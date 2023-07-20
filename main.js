836e8d3dc0b691b6c3ebd36e8fc1b390e4436295
shimamura10 <shimamura-kazuki351@g.ecc.u-tokyo.ac.jp>
2023-07-20 18:05:03 +0900

    タグによる絞り込みをFilterから行うようにした

--- main.js
+++ main.js
@@ -11,24 +11,12 @@
   const addUrl = document.querySelector('#add-url');
   const addTag = document.querySelector('.add-tag');
   const addedTagSpace = document.querySelector('#added-tag-space');
-  const inputSearchTag = document.querySelector('#input-search-tag');
   const searchTagList = document.querySelector('#search-tag-list');
   const sortColumList = document.querySelector('#sort-colum-list');
   const LSkey = 'informationList'; // ローカルストレージのキー
   const targetTags = []; // 絞り込みに使われているタグのリスト
   let informationWindowKey = ''; // ブックマークウィンドウで操作している情報のキー
-
-  // 起動時の処理
-  // ローカルストレージから情報を読み込む
-  let informationList = {};
-  if (!(localStorage[LSkey] == "undefined" || typeof localStorage[LSkey] == "undefined")) {
-    informationList = JSON.parse(localStorage[LSkey]);
-  }
-  // informationListの内容を表示
-  Object.keys(informationList).forEach(key => {
-    displayInformation(key);
-  })
-
+  
   // ブックマークが持つ情報のデフォルト値
   const originInformation = {
     name: '',
@@ -39,12 +27,15 @@
     usedNumber: 0,
   }
 
-  // タグ検索の追加ボタンを押したときの処理
-  document.querySelector('#add-search-tag').addEventListener('click', () => {
-    // タグ要素の追加と表示
-    if (inputSearchTag.value == '') {return;}
-    addSearchTag(inputSearchTag.value);
-    inputSearchTag.value = '';
+  // 起動時の処理
+  // ローカルストレージから情報を読み込む
+  let informationList = {};
+  if (!(localStorage[LSkey] == "undefined" || typeof localStorage[LSkey] == "undefined")) {
+    informationList = JSON.parse(localStorage[LSkey]);
+  }
+  // informationListの内容を表示
+  Object.keys(informationList).forEach(key => {
+    displayInformation(key);
   })
 
   // リストに追加する情報の入力画面を表示
@@ -81,18 +72,17 @@
     // ウィンドウを閉じる
     closeWindow();
   });
-
+  // 登録ウィンドウでタグを増やすボタンの設定
   document.querySelector('#add-tag-button').addEventListener('click', () => {
     cloneTag();
   });
-
-  function cloneTag(num=1) {
-    for (let i = 0; i < num; i++) {
-      const cloneTag = addTag.cloneNode(true);
-      addedTagSpace.appendChild(cloneTag);
-      cloneTag.value = '';
-    }
-  }
+  // filterウィンドウにタグを表示
+  displaySearchTags();
+  // タグ検索ボックスの設定
+  const searchInTags = document.querySelector('#search-in-tags');
+  searchInTags.addEventListener('keyup', () => {
+    displaySearchTags(searchInTags.value);
+  });
 
   // informationをサイト上のリストに追加
   function displayInformation(key) {
@@ -252,10 +242,22 @@
   });
 
   // フィルター関連
+  // フィルターボタンにイベントを設定
+  document.querySelector('#filter-button').addEventListener('click', () => {
+    document.querySelector('#filter-window').classList.toggle('hidden');
+  });
+  // フィルターウィンドウを消すボタンの設定
+  document.querySelector('#filter-window > button').addEventListener('click', () => {
+    document.querySelector('#filter-window').classList.add('hidden');
+  });
+
+  // 絞り込みに使うタグをページに表示する
   function addSearchTag(searchTag) {
+    // 絞り込みタグのhtml要素を作成
     const tag = document.createElement('a');
     tag.classList.add("searchTag");
     tag.text = searchTag;
+    // 絞り込みタグを消すボタンを付ける
     const delButton = document.createElement('input');
     delButton.setAttribute('type', 'button');
     delButton.setAttribute('value', 'X');
@@ -266,12 +268,14 @@
       search();
     })
     tag.appendChild(delButton);
-    searchTagList.appendChild(tag);
-    targetTags.push(tag.text);
-    search();
+    searchTagList.appendChild(tag);  // 作成した絞り込みタグを表示
+    targetTags.push(tag.text); // 絞り込みタグの内部的なリストに追加
+    search(); // 絞り込みタグに従って表示するブックマークを選びなおす
   }
 
+  // searchStringを含むタグのみを表示する
   function displaySearchTags(searchString = '') {
+    // 現存する全タグをtagSetに入れる
     const tagSet = new Set();
     Object.keys(informationList).forEach(key => {
       informationList[key].tag.forEach(tag => {
@@ -279,10 +283,12 @@
       })
     });
     const tagList = Array.from(tagSet);
-    tagList.sort();
-    console.log(tagList);
+    tagList.sort(); // アルファベット順にする
+    // 表示されているタグを全部消す
     const filterTagList = document.querySelector('#filter-tag-list');
     deleteAllChilds(filterTagList);
+    // 全タグのうちsearchStringを含むものだけ表示する
+    // searchStringが空のときは全部表示
     tagList.forEach(tag => {
       if (searchString == '' || tag.indexOf(searchString) != -1){
         const tagli = document.createElement('li');
@@ -295,12 +301,17 @@
       }
     });
   }
-  displaySearchTags();
-  const searchInTags = document.querySelector('#search-in-tags');
-  searchInTags.addEventListener('keyup', () => {
-    displaySearchTags(searchInTags.value);
-  })
 
+  // 登録ウィンドウのタグ入力欄をnum個増やす
+  function cloneTag(num=1) {
+    for (let i = 0; i < num; i++) {
+      const cloneTag = addTag.cloneNode(true);
+      addedTagSpace.appendChild(cloneTag);
+      cloneTag.value = '';
+    }
+  }
+
+  // parentの子要素を全部削除
   function deleteAllChilds(parent) {
     while (parent.firstChild) {
       parent.removeChild(parent.firstChild);
